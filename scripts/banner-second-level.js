e9352f1e0bea5ffe10ca68c32377866782a11137
Gaurav-kapoor <gauravkapur01@gmail.com>
2024-11-15 09:32:42 +0530

    Add files via upload

---
+++ scripts/banner-second-level.js
@@ -0,0 +1,9 @@
+$('#banner-second-level .column-wrapper .slide-wrapper').slick({
+    dots: false,
+    arrows:false,
+    infinite: true,
+    autoplay:true,
+    speed: 1000,
+    slidesToShow: 1,
+    slideToScroll:1
+  });
